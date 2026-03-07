62ee3d1651f05c5768e89f16080b1aad626fb130
Aman RK <85789009+Amanrk7@users.noreply.github.com>
2026-03-07 12:03:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,8 +36,7 @@ import { Logout01Icon } from '@hugeicons/core-free-icons';
 import { TaskEdit01Icon } from '@hugeicons/core-free-icons';
 import { CheckListIcon } from '@hugeicons/core-free-icons';
 import { Moon02Icon, Sun03Icon } from '@hugeicons/core-free-icons';
-// import { ClipboardClockIcon } from '@hugeicons/core-free-icons';
-import { ClipboardClockIcon } from '@hugeicons-pro/core-stroke-rounded';
+import { FolderDotIcon } from '@hugeicons/core-free-icons';
 import PlayerDashboard from "./Pages/PlayerDashboard.jsx";
 import { AddPlayerContext } from "./Context/addPlayer.jsx";
 import { CurrentUserContext } from "./Context/currentUser.jsx";
@@ -323,7 +322,7 @@ const NAV_ITEMS = [
   { id: "dashboard", label: "Dashboard", icon: Home01Icon, adminsOnly: false },
   { id: "memberDashboard", label: "Member Dashboard", icon: CheckListIcon, adminsOnly: false },
   { id: "players", label: "Players", icon: UserGroup03Icon },
-  { id: "dailyCheckups", label: "Daily Checkups", icon: ClipboardClockIcon },
+  { id: "dailyCheckups", label: "Daily Checkups", icon: FolderDotIcon },
   { id: "playTime", label: "Play Time", icon: TimeQuarter02Icon },
   { id: "games", label: "Games", icon: GameboyIcon },
   { id: "attendance", label: "Attendance", icon: Notebook02Icon },
@@ -725,3 +724,4 @@ export default function App() {
     </ThemeProvider>
   );
 }
+
