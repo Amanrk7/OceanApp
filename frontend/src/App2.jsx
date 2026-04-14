96b977e3997f4803a6580db7a6643d9e0bc4e2d0
Aman RK <85789009+Amanrk7@users.noreply.github.com>
2026-04-15 00:21:39 +0530

    Update App2.jsx

--- frontend/src/App2.jsx
+++ frontend/src/App2.jsx
@@ -819,14 +819,14 @@ export default function App() {
                             <Router>
                                 <Routes>
                                     <Route path="/"
-                                        element={user ? <AdminDashboard user={user} /> : <LoginPage />}
+                                        element={!user ? <AdminDashboard user={user} /> : <LoginPage />}
                                     />
                                     <Route path="/PlayerDashboard/:playerId"
                                         element={user ? <PlayerDashboardWithSidebar user={user} /> : <LoginPage />}
                                     />
 
                                     <Route path="/shifts"
-                                        element={!user ? <ShiftsWithSidebar user={user} /> : <LoginPage />}
+                                        element={user ? <ShiftsWithSidebar user={user} /> : <LoginPage />}
                                     />
 
 
