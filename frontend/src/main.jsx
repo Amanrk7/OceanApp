83e44a1fd2cec94ec4141e12b0b74a1894aaa24d
Aman RK <85789009+Amanrk7@users.noreply.github.com>
2026-04-14 14:08:58 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
+import { StrictMode, useContext } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import App2 from './Store2/App2.jsx';
+import { App2Context, App2Provider } from "./Context/store2Switch.jsx";
+
+const { isStore2 } = useContext(App2Context);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App2Provider>
+
+      {!isStore2
+        ? <App /> : <App2 />}
+
+    </App2Provider>
   </StrictMode>,
 )
